ba87deeaeee391f200573a0fb248b8a672bab13b
martinbannasch <m.bannasch@wissenswerft.de>
2024-02-09 14:15:19 +0100

    Grafik push

---
+++ tilesets/iunitTest.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="iunitTest" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="../../../../Downloads/Pipoya RPG Tileset 32x32/iunitTest.png" width="256" height="256"/>
+</tileset>
